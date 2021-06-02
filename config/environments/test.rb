508731da9b56f3281ca39a43248180cb7d803a7f
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2021-06-02 07:07:45 -0300

    Change env of database.yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  ruby "2.7.3"
+  ruby "2.7.2"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
